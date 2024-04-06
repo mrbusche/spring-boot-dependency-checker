d00da96840048f24bb011568dd10a19b6f3e83da
Matt Busche <mrbusche@gmail.com>
2024-04-05 19:56:35 -0500

    get file generation working

---
+++ checker/versions.js
@@ -0,0 +1,34 @@
+const {parse} = require('node-html-parser');
+const {writeFileSync} = require('fs');
+
+const sbVersions = ['3.1.8', '3.1.9']
+sbVersions.forEach(sbVersion =>
+    (async () => {
+        const response = await fetch(`https://docs.spring.io/spring-boot/docs/${sbVersion}/reference/html/dependency-versions.html`);
+        const versions = [];
+        switch (response.status) {
+            // status "OK"
+            case 200:
+                const template = await response.text();
+                const parsedTemplate = parse(template)
+                const tableBody = parsedTemplate.querySelector('table tbody');
+
+                tableBody.childNodes.forEach(child =>
+                    // there's a header row we should skip
+                    child.childNodes.length === 0 ? '' :
+                        versions.push({
+                            group: child.childNodes[1].rawText,
+                            name: child.childNodes[3].rawText,
+                            version: child.childNodes[5].rawText,
+                        })
+                )
+                console.log(versions);
+                writeFileSync(`./${sbVersion}.json`, JSON.stringify(versions));
+                break;
+            // status "Not Found"
+            case 404:
+                console.log('Not Found');
+                break;
+        }
+    })()
+)
