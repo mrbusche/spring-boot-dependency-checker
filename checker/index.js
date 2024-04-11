bf3e2bc852a91f1095ddab7f23872f236e451af4
Matt Busche <mrbusche@gmail.com>
2024-04-10 22:11:22 -0500

    Output mismatched package versions

--- checker/index.js
+++ checker/index.js
@@ -1,5 +1,5 @@
-const { parse } = require('node-html-parser');
-const { existsSync, readFileSync, writeFileSync } = require('fs');
+const {parse} = require('node-html-parser');
+const {existsSync, readFileSync, writeFileSync} = require('fs');
 const path = '.cache';
 
 const getJsonFromFile = async (filename) => {
@@ -26,21 +26,30 @@ const retrieveSimilarPackages = async (bomFile) => {
     const components = await getComponents(bomFile);
     const springBootVersion = await getSpringBootVersion(components);
     console.log('springBootVersion', springBootVersion);
-    const defaultComponents = await getDefaultSpringBootComponents(springBootVersion)
+    const defaultComponents = await getDefaultSpringBootComponents(springBootVersion);
 
-    const matchingPackages = components.filter(bomPackage =>
-        defaultComponents.some(bootPackage =>
-            bomPackage.group === bootPackage.group &&
-            bomPackage.name === bootPackage.name &&
-            bomPackage.version !== bootPackage.version
+    const mismatchedPackages = [];
+    components.forEach(bomPackage =>
+        defaultComponents.forEach(bootPackage => {
+                if (bomPackage.group === bootPackage.group &&
+                    bomPackage.name === bootPackage.name &&
+                    bomPackage.version !== bootPackage.version) {
+                    mismatchedPackages.push({
+                        group: bomPackage.group,
+                        name: bomPackage.name,
+                        bomVersion: bomPackage.version,
+                        bookVersion: bootPackage.version
+                    })
+                }
+            }
         )
     )
+    console.log(mismatchedPackages);
 
     // bom_3.1.9.json line 1139 overwritten
-    console.log('matchingPackages', matchingPackages);
     console.log('components size', components.length);
     console.log('defaultComponents size', defaultComponents.length);
-    console.log('matchingPackages size', matchingPackages.length);
+    console.log('matchingPackages size', mismatchedPackages.length);
 }
 
 const getSpringDefaultVersions = async (sbVersion) => {
@@ -85,10 +94,8 @@ const downloadSpringDefaultVersions = async (sbVersion) => {
     }
 }
 
-(async() => {
-    console.log('before start');
-
+(async () => {
+    const start = Date.now()
     await retrieveSimilarPackages('../samples/bom_3.1.9.json');
-
-    console.log('after start');
+    console.log(`Process took ${Date.now() - start} ms`);
 })();
