74aa8c7679f0b30a6629680af506e931a1e9477b
Matt Busche <mrbusche@gmail.com>
2024-04-27 22:52:19 -0500

    add basic gradle support (#33)

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {
     retrieveSimilarPomPackages,
     retrieveSimilarPomProperties,
 } from './pom.js';
+import { getGradleSpringBootVersion, getJSFromFile, retrieveSimilarGradlePackages } from './gradle.js';
 
 export const checkDependencies = async () => {
     const start = Date.now();
@@ -17,12 +18,21 @@ export const checkDependencies = async () => {
         await retrieveSimilarSbomPackages(filename);
     } else if (fileExtension === '.xml') {
         console.log('Processing xml file');
-        const parsedPom = await getXMLFromFile(process.argv[2]);
+        const parsedPom = await getXMLFromFile(filename);
         const springBootVersion = await getPomSpringBootVersion(parsedPom);
         if (springBootVersion) {
             console.log('Detected Spring Boot Version -', springBootVersion);
             await retrieveSimilarPomPackages(parsedPom, springBootVersion);
             await retrieveSimilarPomProperties(parsedPom, springBootVersion);
+        }
+    } else if (fileExtension === '.gradle') {
+        console.log('Processing gradle file');
+        const parsedGradle = await getJSFromFile(filename);
+        const springBootVersion = await getGradleSpringBootVersion(parsedGradle);
+        if (springBootVersion) {
+            console.log('Detected Spring Boot Version -', springBootVersion);
+            await retrieveSimilarGradlePackages(parsedGradle, springBootVersion);
+            //     await retrieveSimilarPomProperties(parsedPom, springBootVersion);
         }
     } else {
         console.log('Unknown extension, unable to process file.');
