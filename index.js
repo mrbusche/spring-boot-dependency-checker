bea61dcc17c13443caac3e58a6c4f030fb759cf5
Matt Busche <mrbusche@gmail.com>
2026-02-21 11:47:12 -0600

    Add JSON output

--- index.js
+++ index.js
@@ -6,34 +6,44 @@ export const checkDependencies = async () => {
   const start = Date.now();
   const filename = process.argv[2];
   const fileExtension = extname(filename);
-  // console.log('filename', filename, 'fileExtension', fileExtension);
+
+  let result = {
+    fileType: 'Unknown',
+    springBootVersion: null,
+    elapsedMs: 0,
+    packages: [],
+    properties: [],
+  };
+
   if (fileExtension === '.xml') {
-    console.log('Processing xml file');
     const parsedPom = await getXMLFromFile(filename);
     const springBootVersion = await getPomSpringBootVersion(parsedPom);
     if (springBootVersion) {
-      console.log('Detected Spring Boot Version -', springBootVersion);
       const declaredPackages = await retrieveSimilarPomPackages(parsedPom, springBootVersion);
       const declaredProperties = await retrieveSimilarPomProperties(parsedPom, springBootVersion);
-      return {
+      result = {
+        fileType: 'Maven POM',
+        springBootVersion: springBootVersion,
         packages: declaredPackages,
         properties: declaredProperties,
       };
     }
   } else if (fileExtension === '.gradle') {
-    console.log('Processing gradle file');
     const parsedGradle = await getJSFromFile(filename);
     const springBootVersion = await getGradleSpringBootVersion(parsedGradle);
     if (springBootVersion) {
-      console.log('Detected Spring Boot Version -', springBootVersion);
       const declaredPackages = await retrieveSimilarGradlePackages(parsedGradle, springBootVersion);
-      //     await retrieveSimilarPomProperties(parsedPom, springBootVersion);
-      return { packages: declaredPackages, properties: [] };
+      result = {
+        fileType: 'Gradle',
+        springBootVersion: springBootVersion,
+        packages: declaredPackages,
+        properties: [],
+      };
     }
-  } else {
-    console.log('Unknown extension, unable to process file.');
   }
-  console.log(`Process took ${Date.now() - start} ms`);
+
+  result.elapsedMs = Date.now() - start;
+  return result;
 };
 
 // (async () => {
